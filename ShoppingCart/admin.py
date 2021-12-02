c83e96e5c70c10a809f8efa5cb3cce092615ce50
julietaalb <89486467+julietaalb@users.noreply.github.com>
2021-12-01 23:45:29 -0500

    not needed anymore, fixed my issues

--- ShoppingCart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order, OrderItem, Profile
-
-# Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Profile)
